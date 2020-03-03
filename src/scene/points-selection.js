b08dc956b36e7ae9fb43264359f6aee2152cacf6
Daniel Savage <dansvg@gmail.com>
2020-03-03 15:03:34 -0800

    cleaner

--- src/scene/points-selection.js
+++ src/scene/points-selection.js
@@ -5,49 +5,42 @@ const PointsSelectionScreen = function () {
 PointsSelectionScreen.prototype.init = function(payload) {
   //
 };
-PointsSelectionScreen.prototype.create = function() {
-
+PointsSelectionScreen.prototype.create9Slice = function (x, y, width, height) {
   const barsSize = 16;
 
-  const x = 100;
-  const y = 100;
-  const width = 200;
-  const height = 123;
-
-
-  // bottom bar
+  // top bars
   let topCorner = this.add.image(0, 0, 'window_9slice', 0);
   topCorner.setOrigin(0);
-  let topBar = this.add.image(barsSize, 0, 'window_9slice', 1);
+  let topBar = this.add.tileSprite(barsSize, 0, width - (barsSize * 2), 16, 'window_9slice', 1);
   topBar.setOrigin(0);
-  topBar.displayWidth = width - (barsSize * 2);
   let otherTopCorner = this.add.image(width - barsSize, 0, 'window_9slice', 2);
   otherTopCorner.setOrigin(0);
 
   // bottom bars
-  let bottomCorner = this.add.image(0, height - barsSize, 'window_9slice', 6);
+  let bottomCorner =  this.add.image(0, height - barsSize, 'window_9slice', 6);
   bottomCorner.setOrigin(0);
-  let bottomBar = this.add.image(barsSize, height - barsSize, 'window_9slice', 7);
+  let bottomBar = this.add.tileSprite(barsSize, height - barsSize, width - (barsSize * 2), 16, 'window_9slice', 7);
   bottomBar.setOrigin(0);
-  bottomBar.displayWidth = width - (barsSize * 2);
   let otherBottomCorner = this.add.image(width - barsSize, height - barsSize, 'window_9slice', 8);
   otherBottomCorner.setOrigin(0);
 
   // left-right bars
-  let leftBar = this.add.image(0, barsSize, 'window_9slice', 3);
+  let leftBar = this.add.tileSprite(0, barsSize, 16, height - (barsSize * 2), 'window_9slice', 3);
   leftBar.setOrigin(0);
-  leftBar.displayHeight = height - (barsSize * 2);
-  let rightBar = this.add.image((width - barsSize), barsSize, 'window_9slice', 5);
+  let rightBar = this.add.tileSprite(width - barsSize, barsSize, 16, height - (barsSize * 2), 'window_9slice', 5);
   rightBar.setOrigin(0);
-  rightBar.displayHeight = height - (barsSize * 2);
+  
 
   // middle
   let middleZone = this.add.tileSprite(barsSize, barsSize, width - (barsSize * 2), height - (barsSize * 2), 'window_9slice', 4);
   middleZone.setOrigin(0);
 
-  let holder = this.add.container(x, y, [topCorner, topBar, otherTopCorner, bottomCorner, bottomBar, otherBottomCorner, leftBar, rightBar, middleZone]);
+  return this.add.container(x, y, [topCorner, topBar, otherTopCorner, bottomCorner, bottomBar, otherBottomCorner, leftBar, rightBar, middleZone]);
+};
+PointsSelectionScreen.prototype.create = function() {
+  let dialogWindow = this.create9Slice(GAME_WIDTH * 0.1, 32, GAME_WIDTH * 0.2 * 4, GAME_HEIGHT - 64);
 
-  console.log('points select');
+
   const text = this.add.bitmapText(GAME_WIDTH * 0.5, GAME_HEIGHT * 0.5, 'miniset', 'points selection', DEFAULT_TEXT_SIZE);
   text.setCenterAlign();
   text.setOrigin(0.5);
